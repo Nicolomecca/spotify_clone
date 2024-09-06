65088331e5659ec242dfca3dedacc9ae24586c1f
METAORI <nico97.mecca@gmail.com>
2024-09-06 17:23:46 +0200

    end playlist

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -6,19 +6,19 @@ const Playlist = () => {
 
   return (
     <div className="playlist">
-      <h2>Your Playlist</h2>
+      <h2 className='text-white mb-3'>Your Playlist</h2>
       {favourites.length === 0 ? (
         <p>No favorite songs added yet.</p>
       ) : (
-        <Row>
+        <Row className='sidebarMargin'>
           {favourites.map((song) => (
-            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 text-center" key={song.id}>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 text-center" key={song.id} >
               <img 
                 className="img-fluid mb-2" 
                 src={song.album.cover_medium} 
                 alt="track" 
               />
-              <p>
+              <p className='text-white'>
                 Track: "{song.title}"<br />
                 Artist: {song.artist.name}
               </p>
